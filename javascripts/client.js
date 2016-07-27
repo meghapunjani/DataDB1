93fbe9fb5e6444f63d52eba72462710a3f04414a
meghapunjani <megha.punjani@gmail.com>
2016-07-26 23:30:44 -0700

    table test

--- javascripts/client.js
+++ javascripts/client.js
@@ -2,7 +2,15 @@ $.ajax({
   url: "https://data.nasa.gov/resource/gymh-eyc2.json",
   data: {},
   success: function(data) {
-    $("pre").text(JSON.stringify(data, null, "\t"));
+    var content = "<table>"
+    for(i=0; i<3; i++){
+      content += '<tr><td>' + text(JSON.stringify(data, null, "\t") + '</td></tr>';
+    }
+    content += "</table>"
+    $('#data_table').append(content);
+
+
+    //$("pre").text(JSON.stringify(data, null, "\t"));
   },
   error: function(msg) {
     //todo: handle error gracefully
